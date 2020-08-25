03a809b5c979d8c258038d71f71d28034e3f2e42
jbjeong-sstk <jbjeong@shinsungtk.com>
2020-08-25 16:16:11 +0900

    개발환경/ storybook init

--- App.tsx
+++ App.tsx
@@ -1,23 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
-}
+const loadingStorybook = true;
+export default (loadingStorybook
+  ? require('./storybook').default
+  : require('./src/index').default);
---
+++ storybook/index.js
@@ -0,0 +1,23 @@
+import './rn-addons';
+
+import { configure, getStorybookUI } from '@storybook/react-native';
+
+import { AppRegistry } from 'react-native';
+
+// import stories
+configure(() => {
+  require('./stories');
+}, module);
+
+// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({
+  // asyncStorage: require('@react-native-community/async-storage').default || require('react-native').AsyncStorage || null
+  asyncStorage: require('react-native').AsyncStorage || null,
+});
+
+// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
+// If you use Expo you can safely remove this line.
+AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+
+export default StorybookUIRoot;
