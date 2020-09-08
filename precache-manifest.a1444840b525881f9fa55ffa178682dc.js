a4e5197e3c0b07aa6379888f28aabb13a347c952
jbjeong-sstk <jbjeong@shinsungtk.com>
2020-09-08 11:50:49 +0900

    Updates

---
+++ precache-manifest.a1444840b525881f9fa55ffa178682dc.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1872f8c042aa6f302c9",
+    "url": "/dev-sstk/IRinger/static/js/app.e7a202db.chunk.js"
+  },
+  {
+    "revision": "4751d8bfe70005966e4e",
+    "url": "/dev-sstk/IRinger/static/js/runtime~app.943d3887.js"
+  },
+  {
+    "revision": "5c0adcb9b112ef1b63e9",
+    "url": "/dev-sstk/IRinger/static/js/2.b8bf716d.chunk.js"
+  },
+  {
+    "revision": "8ef0d088e8b08cc429c461240e8a850d",
+    "url": "/dev-sstk/IRinger/static/media/iRingerIcon.6ee96ef5.png"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/dev-sstk/IRinger/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/dev-sstk/IRinger/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "9980c07a4a0d38cc3add6da5d93b7e73",
+    "url": "/dev-sstk/IRinger/static/media/star_s.1cd786cb.png"
+  },
+  {
+    "revision": "d60f064cdf0975a868b30ac269ad5040",
+    "url": "/dev-sstk/IRinger/static/media/star_d.758176d2.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/dev-sstk/IRinger/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/dev-sstk/IRinger/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/dev-sstk/IRinger/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dev-sstk/IRinger/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/dev-sstk/IRinger/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/dev-sstk/IRinger/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/dev-sstk/IRinger/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/dev-sstk/IRinger/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/dev-sstk/IRinger/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/dev-sstk/IRinger/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/dev-sstk/IRinger/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "9db1a600a4b2c729a0a12cec5d63770c",
+    "url": "/dev-sstk/IRinger/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/dev-sstk/IRinger/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/dev-sstk/IRinger/expo-service-worker.js"
+  },
+  {
+    "revision": "bacf87b33bcc85dec496469539209665",
+    "url": "/dev-sstk/IRinger/register-service-worker.js"
+  },
+  {
+    "revision": "3349bc92129d2d21eaca008f9cfa03f4",
+    "url": "/dev-sstk/IRinger/static/js/2.b8bf716d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/dev-sstk/IRinger/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/dev-sstk/IRinger/favicon-32.png"
+  },
+  {
+    "revision": "b27287954a8393ef9c70073b2fb0b988",
+    "url": "/dev-sstk/IRinger/favicon.ico"
+  },
+  {
+    "revision": "14030a4ecde042bdfa7357ce20b9b2f1",
+    "url": "/dev-sstk/IRinger/manifest.json"
+  }
+];
